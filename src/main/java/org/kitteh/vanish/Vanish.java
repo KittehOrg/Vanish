06b9d68697e46804c5ee9c018283bb73c951a0e6
mbax <matt@phozop.net>
2016-02-14 13:22:45 -0500

    Version to 4.0.1-SNAPSHOT

--- src/main/java/org/kitteh/vanish/Vanish.java
+++ src/main/java/org/kitteh/vanish/Vanish.java
@@ -33,7 +33,7 @@ import org.spongepowered.api.plugin.Plugin;
 /**
  * Spiritual successor to VanishNoPacket, for Sponge.
  */
-@Plugin(id = "Vanish", name = "Vanish", version = "4.0.0")
+@Plugin(id = "Vanish", name = "Vanish", version = "4.0.1-SNAPSHOT")
 public class Vanish {
     public static final String PERMISSION_VANISH = "vanish.vanish";
 
